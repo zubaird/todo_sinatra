a8571ba1a991f93d112eff5b374aed0b8a2b3999
Kane Baccigalupi <baccigalupi@gmail.com>
2014-11-12 11:24:12 -0800

    a lot of methods extracted

--- lib/todo_app.rb
+++ lib/todo_app.rb
@@ -6,49 +6,85 @@ class TodoApp < CommandLineApp
     @tasks = []
   end
 
-  def run
+  def print_project_menu
     puts "Welcome"
     puts "'list' to list projects"
     puts "'create' to create a new project"
     puts "'edit' to edit a project"
     puts "'rename' to rename a project"
     puts "'delete' to rename a project"
+  end
+
+  def print_projects_list
+    puts "Projects:\n  #{print_projects} "
+  end
+
+  def print_project_create_prompt
+    puts "Please enter the new project name:\n"
+  end
+
+  def print_project_delete_prompt
+    puts "Please enter the project name to delete:\n"
+  end
+
+  def print_project_rename_prompt
+    puts "Please enter the project name to rename:\n"
+  end
+
+  def print_prompt_for_new_project_name
+    puts "Please enter the new project name:\n"
+  end
+
+  def project_add(name)
+    @projects << name
+  end
+
+  def project_delete(name)
+    if project_present?(name)
+      @projects.delete(name)
+    end
+  end
+
+  def project_present?(name)
+    @projects.include?(name)
+  end
+
+  def project_rename(old_name, new_name)
+    @projects.delete(old_name)
+    @projects << new_name
+  end
+
+  def get_input
+    gets.chomp
+  end
+
+  def run
+    print_project_menu
 
     welcome_menu = true
 
-
     while welcome_menu
-      input = gets.chomp
+      input = get_input
+
       if input == 'list'
-        puts "Projects:\n  #{print_projects} "
-
+        print_projects_list
       elsif input == 'create'
-        puts "Please enter the new project name:\n"
-        create_project = gets.chomp
-        @projects << create_project
-        run
-
+        print_project_create_prompt
+        project_add(get_input)
+        print_project_menu
       elsif input == 'delete'
-        puts "Please enter the project name to delete:\n"
-        delete_project = gets.chomp
-        if @projects.include?(delete_project)
-          @projects.delete(delete_project)
-        end
-
+        print_project_delete_prompt
+        project_delete(get_input)
       elsif input == 'rename'
-        puts "Please enter the project name to rename:\n"
-        rename_project = gets.chomp
-        if @projects.include?(rename_project)
-          puts "Please enter the new project name:\n"
-          new_project_name = gets.chomp
-          @projects.delete(rename_project)
-          @projects << new_project_name
+        print_project_rename_prompt
+        if project_present?(old_name = get_input)
+          print_prompt_for_new_project_name
+          project_rename(old_name, get_input)
         end
-
       elsif input == 'edit'
         puts "Which project would you like to edit?\n"
         puts "Projects:\n  *#{print_projects} "
-        edit_project = gets.chomp
+        edit_project = get_input
         if @projects.include?(edit_project)
           puts "Editing Project: #{print_projects} "
           puts "'list' to list tasks"
@@ -59,19 +95,19 @@ class TodoApp < CommandLineApp
 
           task_menu = true
           while task_menu
-            task_input = gets.chomp
+            task_input = get_input
             if task_input == 'list'
               puts "  #{list_tasks}"
             elsif task_input == 'create'
               puts "Please enter the task you would like to add."
-              new_task = gets.chomp
+              new_task = get_input
               @tasks << new_task
             elsif task_input == 'edit'
               puts "Please enter the task you would like to edit."
-              edit_task = gets.chomp
+              edit_task = get_input
               if @tasks.include?(edit_task)
                 puts "Please enter the new task name:\n"
-                new_task_name = gets.chomp
+                new_task_name = get_input
                 @tasks.delete(edit_task)
                 @tasks << new_task_name
               else
@@ -80,7 +116,7 @@ class TodoApp < CommandLineApp
             elsif task_input == 'complete'
               puts "Which task have you completed?"
               puts "  #{list_tasks}"
-              complete_task = gets.chomp
+              complete_task = get_input
               if @tasks.include?(complete_task)
                 @tasks.delete(complete_task)
                 complete_task = "#{complete_task}: completed"
@@ -89,7 +125,7 @@ class TodoApp < CommandLineApp
                 puts "task not found: 'not here'"
               end
             elsif task_input == 'back'
-              run
+              print_project_menu
               task_menu = false
             elsif task_input == 'quit'
               welcome_menu = false
