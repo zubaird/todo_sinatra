a9103aa9a90a1b7df4d99dc097d723058c833e59
Kane Baccigalupi <baccigalupi@gmail.com>
2014-11-13 11:40:25 -0800

    extracts big ugly view class

--- lib/todo_app.rb
+++ lib/todo_app.rb
@@ -13,76 +13,62 @@ class TodoApp < CommandLineApp
   end
 
   def print_project_menu
-    puts "Welcome"
-    puts "'list' to list projects"
-    puts "'create' to create a new project"
-    puts "'edit' to edit a project"
-    puts "'rename' to rename a project"
-    puts "'delete' to rename a project"
+    View.new(output).print_project_menu
   end
 
   def print_task_menu(project_name)
-    puts "Editing Project: #{project_name} "
-    puts "'list' to list tasks"
-    puts "'create' to create a new task"
-    puts "'edit' to edit a task"
-    puts "'complete' to complete a task and remove it from the list"
+    View.new(output).print_task_menu(project_name)
   end
 
   def print_projects_list(projects)
-    puts "Projects:"
-    if projects.empty?
-      puts "  none"
-    else
-      puts "  #{projects.join(', ')}"
-    end
+    View.new(output).print_projects_list(projects)
   end
 
   def print_tasks_list(tasks)
-    puts "Tasks:"
-    if tasks.empty?
-      puts "  none"
-    else
-      puts "  #{tasks.join(', ')}"
-    end
+    View.new(output).print_tasks_list(tasks)
   end
 
+  # V, printer
   def print_project_create_prompt
-    puts "Please enter the new project name:\n"
+    View.new(output).print_project_create_prompt
   end
 
+  # V, printer
   def print_project_delete_prompt
-    puts "Please enter the project name to delete:\n"
+    View.new(output).print_project_delete_prompt
   end
 
   def print_project_rename_prompt
-    puts "Please enter the project name to rename:\n"
+    View.new(output).print_project_rename_prompt
   end
 
   def print_prompt_for_new_project_name
-    puts "Please enter the new project name:\n"
+    View.new(output).print_prompt_for_new_project_name
   end
 
   def print_project_edit_prompt
-    puts "Which project would you like to edit?\n"
+    View.new(output).print_project_edit_prompt
   end
 
   def print_task_edit_prompt
-    puts "Please enter the task you would like to edit."
+    View.new(output).print_task_edit_prompt
   end
 
   def print_prompt_for_new_task_name
-    puts "Please enter the new task name:\n"
-  end
-
-  def print_task_not_here_message(name)
-    puts "task not found: '#{name}'"
+    View.new(output).print_prompt_for_new_task_name
   end
 
   def print_new_task_prompt
-    puts "Please enter the task you would like to add."
+    View.new(output).print_new_task_prompt
   end
 
+  def print_task_not_here_message(name)
+    View.new(output).print_task_not_here_message(name )
+  end
+
+  # ------------
+
+  # M, projects collection
   def project_add(name)
     projects << name
   end
@@ -102,6 +88,7 @@ class TodoApp < CommandLineApp
     projects << new_name
   end
 
+  # M, tasks collection
   def add_task(name)
     tasks << name
   end
@@ -121,7 +108,12 @@ class TodoApp < CommandLineApp
     tasks << completed_task
   end
 
+  # router
   def run
+    run_project_menu
+  end
+
+  def run_project_menu
     print_project_menu
 
     welcome_menu = true
---
+++ lib/view.rb
@@ -0,0 +1,85 @@
+class View
+  def initialize(output)
+    @output = output
+  end
+
+  def output
+    @output
+  end
+
+  def print_project_menu
+    puts "Welcome"
+    puts "'list' to list projects"
+    puts "'create' to create a new project"
+    puts "'edit' to edit a project"
+    puts "'rename' to rename a project"
+    puts "'delete' to rename a project"
+  end
+
+  def print_task_menu(project_name)
+    puts "Editing Project: #{project_name} "
+    puts "'list' to list tasks"
+    puts "'create' to create a new task"
+    puts "'edit' to edit a task"
+    puts "'complete' to complete a task and remove it from the list"
+  end
+
+  def print_projects_list(projects)
+    puts "Projects:"
+    if projects.empty?
+      puts "  none"
+    else
+      puts "  #{projects.join(', ')}"
+    end
+  end
+
+  def print_tasks_list(tasks)
+    puts "Tasks:"
+    if tasks.empty?
+      puts "  none"
+    else
+      puts "  #{tasks.join(', ')}"
+    end
+  end
+
+  def print_project_create_prompt
+    puts "Please enter the new project name:\n"
+  end
+
+  # V, printer
+  def print_project_delete_prompt
+    puts "Please enter the project name to delete:\n"
+  end
+
+  def print_project_rename_prompt
+    puts "Please enter the project name to rename:\n"
+  end
+
+  def print_prompt_for_new_project_name
+    puts "Please enter the new project name:\n"
+  end
+
+  def print_project_edit_prompt
+    puts "Which project would you like to edit?\n"
+  end
+
+  def print_task_edit_prompt
+    puts "Please enter the task you would like to edit."
+  end
+
+  def print_prompt_for_new_task_name
+    puts "Please enter the new task name:\n"
+  end
+
+  def print_new_task_prompt
+    puts "Please enter the task you would like to add."
+  end
+
+  def print_task_not_here_message(name)
+    puts "task not found: '#{name}'"
+  end
+
+  def puts(message)
+    output.puts(message)
+  end
+end
